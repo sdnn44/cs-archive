de786107919542e4b798492d6534f6ebdd949940
sdnn44 <slidennnq@gmail.com>
2024-02-14 20:46:09 +0100

    Improved button responsiveness

--- frontend/src/components/Playerbase/PlayerAmount.jsx
+++ frontend/src/components/Playerbase/PlayerAmount.jsx
@@ -8,15 +8,19 @@ export const PlayerAmount = () => {
     <div className="player-amount-container">
       <div className="player-amount">
         <div className="pulse">
-          <PersonOffIcon sx={{fontSize: "3.5rem"}}/>
+          <PersonOffIcon sx={{ fontSize: "3.5rem" }} />
         </div>{" "}
-        <div className="player-amount-text"><div className="count">3158</div> graczy</div>
+        <div className="player-amount-text">
+          <div className="count">3158</div> graczy
+        </div>
       </div>
       <div className="website-amount">
         <div className="pulse">
-          <LanguageIcon sx={{fontSize: "3.5rem"}} />
+          <LanguageIcon sx={{ fontSize: "3.5rem" }} />
         </div>{" "}
-        <div className="website-amount-text"><div className="count">13</div> sieci</div>
+        <div className="website-amount-text">
+          <div className="count">13</div> sieci
+        </div>
       </div>
     </div>
   );
