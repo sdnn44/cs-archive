677d73621d327f8dae6be773f6230ac3414014be
sdnn44 <slidennnq@gmail.com>
2024-05-19 01:08:09 +0200

    Updated amount of banned players

--- frontend/src/components/PlayerList/PlayerDetails.jsx
+++ frontend/src/components/PlayerList/PlayerDetails.jsx
@@ -21,7 +21,7 @@ export const PlayerDetails = () => {
         <a href="https://cs-archive.vercel.app/" className="page-back">
           <ArrowBackIcon sx={{ fontSize: "3rem" }} className="arrow-back" />
         </a>
-        <span>Ostatnia aktualizacja bazy danych z banami graczy nastąpiła 6 maja br. o godzinie 17:48.</span>
+        <span>Ostatnia aktualizacja bazy danych z banami graczy nastąpiła 18 maja br. o godzinie 23:48.</span>
       </div>
       <BanList />
       <PlayerStatus />
